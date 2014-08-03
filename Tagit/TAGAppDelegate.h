36f8cb8b7f5a9e3b966ee854d914f9e4c36e9712
Shane Rogers <rogerssh@tcd.ie>
2014-08-02 20:46:07 -0700

    Project initialization

---
+++ Tagit/TAGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TAGAppDelegate.h
+//  Tagit
+//
+//  Created by Shane Rogers on 8/2/14.
+//  Copyright (c) 2014 Shane Rogers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TAGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tagit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tagit
+//
+//  Created by Shane Rogers on 8/2/14.
+//  Copyright (c) 2014 Shane Rogers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TAGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TAGAppDelegate class]));
+    }
+}
