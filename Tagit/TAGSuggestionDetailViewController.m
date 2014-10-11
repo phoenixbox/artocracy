a7a4cb7bd18eb3c658eb065fcc8c55514e5ca13d
Shane Rogers <rogerssh@tcd.ie>
2014-10-11 00:08:00 -0700

    Fetch and render proposals in parent detail view

--- Tagit/TAGSuggestionDetailViewController.m
+++ Tagit/TAGSuggestionDetailViewController.m
@@ -17,6 +17,7 @@
 #import "TAGSuggestionDetailsSection.h"
 #import "TAGLateralTableViewCell.h"
 #import "TAGMapAnnotation.h"
+#import "TAGErrorAlert.h"
 
 // Constants
 #import "TAGStyleConstants.h"
@@ -26,31 +27,37 @@
 // Pods
 #import "URBMediaFocusViewController.h"
 
+// Data Layer
+#import "TAGProposalChannel.h"
+#import "TAGSuggestionStore.h"
+
 @interface TAGSuggestionDetailViewController ()
 
 // Data Layer
 @property (nonatomic, strong)TAGSuggestion *_suggestion;
 // Header section
-@property (nonatomic, strong)UIView *_header;
-@property (nonatomic, strong)UIView *_userThumbnail;
-@property (nonatomic, strong)UILabel *_userName;
-@property (nonatomic, strong)UILabel *_upvoteCounter;
+@property (nonatomic, strong) UIView *_header;
+@property (nonatomic, strong) UIView *_userThumbnail;
+@property (nonatomic, strong) UILabel *_userName;
+@property (nonatomic, strong) UILabel *_upvoteCounter;
 // Main Content
-@property (nonatomic, strong)UIImageView *_suggestionImage;
+@property (nonatomic, strong) UIImageView *_suggestionImage;
 // Map view
 @property (nonatomic, strong) MKMapView *_mapView;
 // Suggestion Detail Section
-@property (nonatomic, strong)TAGSuggestionDetailsSection *_suggestionDetailsSection;
+@property (nonatomic, strong) TAGSuggestionDetailsSection *_suggestionDetailsSection;
 // Comment action
-@property (nonatomic, strong)UIButton *_commentButton;
+@property (nonatomic, strong) UIButton *_commentButton;
 // Lateral Table
-@property (nonatomic, strong)UILabel *_proposalTitle;
-@property (nonatomic, strong)UILabel *_proposalCount;
-@property (nonatomic, strong)UITableView *_proposalsTable;
-@property (nonatomic, strong)URBMediaFocusViewController *_lightboxViewController;
-@property (nonatomic, assign)float _cellDimension;
+@property (nonatomic, strong) UIActivityIndicatorView *_activityIndicator;
+@property (nonatomic, strong) TAGProposalChannel *_proposalChannel;
+@property (nonatomic, strong) UILabel *_proposalTitle;
+@property (nonatomic, strong) UILabel *_proposalCount;
+@property (nonatomic, strong) UITableView *_proposalsTable;
+@property (nonatomic, strong) URBMediaFocusViewController *_lightboxViewController;
+@property (nonatomic, assign) float _cellDimension;
 // Container scroll view
-@property (nonatomic, strong)UIScrollView *_scrollView;
+@property (nonatomic, strong) UIScrollView *_scrollView;
 
 @end
 
@@ -92,6 +99,8 @@
     [self renderMap];
 
     [self renderSuggestionDetailsContainer];
+
+    [self fetchProposalsForSuggestion];
     [self renderProposalsTable];
 }
 
@@ -243,6 +252,23 @@
     [self.view addSubview:self._suggestionDetailsSection];
 }
 
+- (void)fetchProposalsForSuggestion {
+    self._activityIndicator = [TAGViewHelpers setActivityIndicatorForNavItem:[self navigationItem]];
+
+    void(^completionBlock)(TAGProposalChannel *obj, NSError *err)=^(TAGProposalChannel *obj, NSError *err){
+        if(!err){
+            self._proposalChannel = obj;
+            [self._proposalsTable reloadData];
+        } else {
+            [TAGErrorAlert render:err];
+        }
+        [self._activityIndicator stopAnimating];
+    };
+
+    [[TAGSuggestionStore sharedStore] fetchProposalsForSuggestion:self._suggestion.id withCompletionBlock:completionBlock];
+
+}
+
 - (void)renderProposalsTable {
     [self renderTableHeader];
     float yCoord = CGRectGetMaxY(self._proposalTitle.frame) + kSmallPadding;
@@ -261,7 +287,7 @@
     self._proposalsTable.alwaysBounceVertical = NO;
     self._proposalsTable.scrollEnabled = YES;
     self._proposalsTable.separatorInset = UIEdgeInsetsMake(0, 3, 0, 3);
-    self._proposalsTable.separatorColor = [UIColor blackColor];
+    self._proposalsTable.separatorColor = [UIColor clearColor];
 
     [self._proposalsTable setBackgroundColor:[UIColor whiteColor]];
 
@@ -295,15 +321,38 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 10;
+    NSUInteger rowCount = [self._proposalChannel.proposals count];
+
+    if (rowCount > 0) {
+        return rowCount;
+    } else {
+        // TODO: Maybe rotate???
+        UILabel *messageLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height)];
+        [TAGViewHelpers formatLabel:messageLabel withCopy:@"no proposals have been made"];
+        messageLabel.textColor = [UIColor blackColor];
+        messageLabel.numberOfLines = 0;
+        messageLabel.textAlignment = NSTextAlignmentCenter;
+        [messageLabel sizeToFit];
+
+        self._proposalsTable.backgroundView = messageLabel;
+    }
+    return 0;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    TAGLateralTableViewCell *cell = (TAGLateralTableViewCell *)[tableView dequeueReusableCellWithIdentifier:kTAGLateralTableViewCellIdentifier];
+    TAGLateralTableViewCell *cell = [[TAGLateralTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:kTAGLateralTableViewCellIdentifier forCellDimension:self._cellDimension];
 
     if([tableView isEqual:self._proposalsTable]){
-        cell = [[TAGLateralTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:kTAGLateralTableViewCellIdentifier forCellDimension:self._cellDimension];
-        [cell addImage:@"open_arms_SF.png"];
+        UIImageView *backgroundImage = [UIImageView new];
+        if ([self._proposalChannel.proposals count] > 0) {
+            TAGProposal *proposal = [self._proposalChannel.proposals objectAtIndex:[indexPath row]];
+
+            UIImage *img = [TAGViewHelpers imageForURL:proposal.imageUrl];
+
+            [backgroundImage setImage:img];
+        }
+        [cell setBackgroundView:backgroundImage];
+        [cell.backgroundView setContentMode:UIViewContentModeScaleAspectFit];
         [cell setSelectionStyle:UITableViewCellSelectionStyleNone];
     }
     return cell;
