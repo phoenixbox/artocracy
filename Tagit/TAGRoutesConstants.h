082a0aa5faa20b076e8e14d730b5b71db1e5421f
Shane Rogers <rogerssh@tcd.ie>
2014-12-19 09:06:08 +0000

    update the router and the login view controller

--- Tagit/TAGRoutesConstants.h
+++ Tagit/TAGRoutesConstants.h
@@ -55,26 +55,26 @@
 //#define kAPIFavoriteCreate @"http://192.168.0.100:3000/api/v1/favorites"
 //#define kAPIFavoriteDestroy @"http://192.168.0.100:3000/api/v1/favorites"
 
-// Tahoe
-#define kAPIUserLogin @"http://192.168.1.138:3000/api/v1/users/login"
-#define kAPIUserContributionCounts @"http://192.168.1.138:3000/api/v1/users"
-#define kAPIUserFavorites @"http://192.168.1.138:3000/api/v1/users"
+// Ardilea
+#define kAPIUserLogin @"http://192.168.1.11:3000/api/v1/users/login"
+#define kAPIUserContributionCounts @"http://192.168.1.11:3000/api/v1/users"
+#define kAPIUserFavorites @"http://192.168.1.11:3000/api/v1/users"
 
-#define kAPIPiecesIndex @"http://192.168.1.138:3000/api/v1/tags"
-#define kAPIPiecesArtistWork @"http://192.168.1.138:3000/api/v1/tags/artist_work"
-#define kAPIPiecesCreate @"http://192.168.1.138:3000/api/v1/tags"
+#define kAPIPiecesIndex @"http://192.168.1.11:3000/api/v1/tags"
+#define kAPIPiecesArtistWork @"http://192.168.1.11:3000/api/v1/tags/artist_work"
+#define kAPIPiecesCreate @"http://192.168.1.11:3000/api/v1/tags"
 
-#define kAPISuggestionsCreate @"http://192.168.1.138:3000/api/v1/suggestions"
-#define kAPISuggestionsIndex @"http://192.168.1.138:3000/api/v1/suggestions"
-#define kAPISuggestionsProposalsIndex @"http://192.168.1.138:3000/api/v1/suggestions"
+#define kAPISuggestionsCreate @"http://192.168.1.11:3000/api/v1/suggestions"
+#define kAPISuggestionsIndex @"http://192.168.1.11:3000/api/v1/suggestions"
+#define kAPISuggestionsProposalsIndex @"http://192.168.1.11:3000/api/v1/suggestions"
 
-#define kAPIUpvoted @"http://192.168.1.138:3000/api/v1/upvoted"
-#define kAPIUpvoteCreate @"http://192.168.1.138:3000/api/v1/upvotes"
-#define kAPIUpvoteDestroy @"http://192.168.1.138:3000/api/v1/upvotes"
+#define kAPIUpvoted @"http://192.168.1.11:3000/api/v1/upvoted"
+#define kAPIUpvoteCreate @"http://192.168.1.11:3000/api/v1/upvotes"
+#define kAPIUpvoteDestroy @"http://192.168.1.11:3000/api/v1/upvotes"
 
-#define kAPIFavorited @"http://192.168.1.138:3000/api/v1/favorited"
-#define kAPIFavoriteCreate @"http://192.168.1.138:3000/api/v1/favorites"
-#define kAPIFavoriteDestroy @"http://192.168.1.138:3000/api/v1/favorites"
+#define kAPIFavorited @"http://192.168.1.11:3000/api/v1/favorited"
+#define kAPIFavoriteCreate @"http://192.168.1.11:3000/api/v1/favorites"
+#define kAPIFavoriteDestroy @"http://192.168.1.11:3000/api/v1/favorites"
 
 
 // Tethered
